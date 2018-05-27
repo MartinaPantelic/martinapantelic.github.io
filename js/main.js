9e5d8f8e878337d80fa681d0a5b917849a67f8aa
Martina Savic <smartinans@gmail.com>
2018-05-27 17:04:45 +0200

    cv redesign

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
         $(document).ready(function() {
             $('#fullpage').fullpage({
-                anchors: ['firstPage', 'secondPage', '3rdPage','4thPage','5thPage','6thPage','7thPage','8thPage','9thPage'],
+                anchors: ['firstPage', 'secondPage', '3rdPage','5thPage','7thPage','8thPage','9thPage'],
                 navigation: true,
                 navigationPosition: 'right',
                 menu: '#menu',
