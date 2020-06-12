79b00456bac3eff62e72f36a7831998282f35207
Martina Savic <smartinans@gmail.com>
2020-06-12 11:38:47 -0500

    cleaning repository

--- js/main.js
+++
@@ -1,9 +0,0 @@
-        $(document).ready(function() {
-            $('#fullpage').fullpage({
-                anchors: ['firstPage', 'secondPage', '3rdPage','4thPage','5thPage','6thPage','7thPage'],
-                navigation: true,
-                navigationPosition: 'right',
-                menu: '#menu',
-            continuousVertical: true
-            });
-        });
